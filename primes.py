704a9fd3cabe9008f746f0a27dd23876056ace73
George Korodimos <61911511+LuxionDev@users.noreply.github.com>
2022-10-18 14:38:51 +0000

    completed exercise

--- primes.py
+++ primes.py
@@ -2,5 +2,20 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 def primes(number_of_primes):
-    list = []
+
+    if number_of_primes <= 0:
+        raise ValueError
+
+    list = [2]
+    i = 3
+    while len(list) < number_of_primes:
+        prime = True
+        for j in list:
+            if i%j == 0:
+                prime = False
+                break
+        if prime:
+            list.append(i)
+        i += 2
+    print(list)
     return list
